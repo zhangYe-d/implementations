7dfa5a154e7de5db5c858024521fec28d9b98244
zhangye <13244619815@163.com>
2022-10-22 23:40:12 +0800

    react: 实现render和commit阶段相关逻辑

--- src/react-dom/ReactDom.js
+++ src/react-dom/ReactDom.js
@@ -1,22 +1,122 @@
-const render = (element, container) => {
+const createDom = fiber => {
 	const HtmlElement =
-		element.type === 'TEXT_ELEMENT'
+		fiber.type === 'TEXT_ELEMENT'
 			? document.createTextNode('')
-			: document.createElement(element.type)
+			: document.createElement(fiber.type)
 
 	const isProperty = propName => propName !== 'children'
 
-	Object.keys(element.props)
+	Object.keys(fiber.props)
 		.filter(isProperty)
 		.forEach(propName => {
-			HtmlElement[propName] = element.props[propName]
+			HtmlElement[propName] = fiber.props[propName]
 		})
 
-	element.props.children.forEach(child => {
-		render(child, HtmlElement)
-	})
+	return HtmlElement
+}
 
-	container.appendChild(HtmlElement)
+const performUnitOfWork = fiber => {
+	if (!fiber.dom) {
+		fiber.dom = createDom(fiber)
+	}
+
+	if (fiber.parent) {
+		fiber.parent.dom.appendChild(fiber.dom)
+	}
+
+	const elements = fiber.props.children
+	let index = 0
+	let prevSibling = null
+
+	while (index < elements.length) {
+		const element = elements[index]
+
+		const newFiber = {
+			type: element.type,
+			props: element.props,
+			parent: fiber,
+			dom: null,
+		}
+
+		if (index === 0) {
+			fiber.child = newFiber
+		} else {
+			prevSibling.sibling = newFiber
+		}
+
+		prevSibling = newFiber
+
+		index++
+	}
+
+	if (fiber.child) {
+		return fiber.child
+	}
+
+	let nextFiber = fiber
+
+	while (nextFiber) {
+		if (nextFiber.sibling) {
+			return nextFiber.sibling
+		}
+
+		nextFiber = nextFiber.parent
+	}
+}
+
+let nextUnitOfWork = null
+let workInProgressRoot = null
+
+const commitWork = fiber => {
+	if (!fiber) {
+		return
+	}
+
+	const parentDom = fiber.parent.dom
+	parentDom.appendChild(fiber.dom)
+
+	commitWork(fiber.child)
+	commitWork(fiber.sibling)
+}
+
+const commitRoot = () => {
+	commitWork(workInProgressRoot.child)
+
+	workInProgressRoot = null
+}
+
+const workLoop = deadline => {
+	let shouldYield = false
+
+	while (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+		shouldYield = deadline.timeRemaining() < 1
+	}
+
+	if (!nextUnitOfWork && workInProgressRoot) {
+		commitRoot()
+	}
+
+	requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+const render = (element, container) => {
+	workInProgressRoot = {
+		dom: container,
+		props: {
+			children: [element],
+		},
+	}
+
+	nextUnitOfWork = workInProgressRoot
+
+	// element.props.children.forEach(child => {
+	// 	render(child, HtmlElement)
+	// })
+
+	// container.appendChild(HtmlElement)
 }
 
 export default { render }
