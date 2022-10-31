cf82f144b5c90ed09bbd04fc97bfd97721e44253
zhangye <13244619815@163.com>
2022-10-31 18:44:12 +0800

    react_feat: 实现useReducer和useState

--- src/react/ReactFiberHooks.js
+++ src/react/ReactFiberHooks.js
@@ -1,10 +1,10 @@
 let workInProgressHook = null
-let curentFiber = null
+let curentRenderedFiber = null
 let currentHook = null
 
 const renderWithHook = fiber => {
 	workInProgressHook = null
-	curentFiber = fiber
+	curentRenderedFiber = fiber
 	currentHook = null
 }
 
@@ -18,7 +18,7 @@ const mountWorkInProgressHook = () => {
 	}
 
 	if (workInProgressHook === null) {
-		curentFiber.memoizedState = workInProgressHook = hook
+		curentRenderedFiber.memoizedState = workInProgressHook = hook
 	} else {
 		workInProgressHook = workInProgressHook.next = hook
 	}
@@ -30,7 +30,7 @@ const updateWorkInProgressHook = () => {
 	let nextCurrentHook = null
 
 	if (currentHook === null) {
-		nextCurrentHook = curentFiber.alternate.memoizedState
+		nextCurrentHook = curentRenderedFiber.alternate.memoizedState
 	} else {
 		nextCurrentHook = currentHook.next
 	}
@@ -46,7 +46,7 @@ const updateWorkInProgressHook = () => {
 	}
 
 	if (workInProgressHook === null) {
-		workInProgressHook = curentFiber.memoizedState = newHook
+		workInProgressHook = curentRenderedFiber.memoizedState = newHook
 	} else {
 		workInProgressHook = workInProgressHook.next = newHook
 	}
@@ -54,10 +54,102 @@ const updateWorkInProgressHook = () => {
 	return workInProgressHook
 }
 
+const dispatchReducerAction = (fiber, queue, action) => {
+	const update = {
+		action,
+		next: null,
+	}
+
+	if (queue.pendding === null) {
+		update.next = update
+	} else {
+		update.next = update.pendding.next
+		update.pendding.next = update
+	}
+
+	queue.pendding = update
+}
+
 const mountReducer = (reducer, initialArg, init) => {
 	const hook = mountWorkInProgressHook()
+
+	const initial = init ? init(initialArg) : initialArg
+	hook.memoisedState = hook.baseState = initial
+
+	const queue = {
+		pendding: null,
+		lastRenderReducer: reducer,
+		lastRenderState: initial,
+		dispatch: null,
+	}
+
+	const dispatch = (queue.dispatch = dispatchReducerAction.bind(
+		null,
+		curentRenderedFiber,
+		queue
+	))
+
+	return [hook.memoizedState, dispatch]
 }
 
 const updateReducer = (reducer, initialArg, init) => {
 	const hook = updateWorkInProgressHook()
+	hook.memoizedState = currentHook.queue.lastRenderState
+
+	let penddingQueue = currentHook.queue.pendding.next
+	currentHook.queue.pendding.next = null
+	const lastRenderReducer = currentHook.queue.lastRenderReducer
+
+	while (penddingQueue) {
+		hook.memoizedState = lastRenderReducer(hook.memoisedState, action)
+
+		penddingQueue = penddingQueue.next
+	}
+
+	hook.queue = {
+		pendding: null,
+		lastRenderReducer: reducer,
+		lastRenderState: hook.memoizedState,
+		dispatch: null,
+	}
+
+	const dispatch = (queue.dispatch = dispatchReducerAction.bind(
+		null,
+		curentRenderedFiber,
+		queue
+	))
+
+	return [hook.memoizedState, dispatch]
 }
+
+const BasicReducer = (state, action) => {
+	return typeof action === 'function' ? action(state) : action
+}
+
+const mountState = initialState => {
+	const hook = mountWorkInProgressHook()
+
+	hook.memoizedState =
+		typeof initialState === 'function' ? initialState() : initialState
+
+	const queue = {
+		pendding: null,
+		lastRenderReducer: BasicReducer,
+		lastRenderState: hook.memoizedState,
+		dispatch: null,
+	}
+
+	const dispatch = (update.dispatch = dispatchReducerAction.bind(
+		null,
+		curentRenderedFiber,
+		queue
+	))
+
+	return [hook.memoizedState, dispatch]
+}
+
+const updateState = state => {
+	return updateReducer(BasicReducer, state)
+}
+
+export {}
