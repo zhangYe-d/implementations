08d60ea903c7b5c1f86c00e2d09f31100940755a
zhangye <13244619815@163.com>
2022-11-24 00:41:11 +0800

    redux_feat: 实现createStore方法

--- packages/redux/index.js
+++ packages/redux/index.js
@@ -1,4 +1,37 @@
-function createStore() {}
+function createEvent() {
+	let handlers = []
+
+	return {
+		get length() {
+			return handlers.length
+		},
+
+		listen(callback) {
+			handlers.push(callback)
+			return () => {
+				handlers = handlers.filter(handler => handler !== callback)
+			}
+		},
+	}
+}
+
+function createStore(initialReducer) {
+	let state = null
+	let reducer = initialReducer
+
+	const event = createEvent()
+
+	function dispatch(action) {
+		state = reducer(state, action)
+	}
+
+	return {
+		dispatch,
+		getState: () => state,
+		subscribe: event.listen,
+		replaceReducer: newReducer => (reducer = newReducer),
+	}
+}
 
 function applyMiddleware() {}
 
