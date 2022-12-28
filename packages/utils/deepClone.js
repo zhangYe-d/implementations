4cb4ecb423fa49e6843393ce41529e8a5f40d01d
zhangye <13244619815@163.com>
2022-12-28 17:58:48 +0800

    utils_feat: 实现深拷贝

---
+++ packages/utils/deepClone.js
@@ -0,0 +1,26 @@
+/**
+ * 深拷贝
+ * @param {any} object 被拷贝的对象
+ */
+function deepClone(object, map = new Map()) {
+	if (typeof object !== 'object') {
+		return object
+	}
+
+	if (map.get(object)) {
+		return map.get(object)
+	}
+
+	let result = {}
+	if (Array.isArray(object)) {
+		result = []
+	}
+
+	for (let key in object) {
+		if (Object.hasOwn(object, key)) {
+			result[key] = deepClone(object[key], map)
+		}
+	}
+
+	return result
+}
